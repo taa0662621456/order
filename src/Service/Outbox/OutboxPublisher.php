e591e871540d172d3a1b8d3c76f5532bc31c8ae4
Oleksandr Tishchenko <taa0662621456@gmail.com>
2025-10-07 01:07:37 -0500

    OrderComponent_v1.0.0-rc1-RabbitMQ_Tests-extends

--- src/Service/Outbox/OutboxPublisher.php
+++ src/Service/Outbox/OutboxPublisher.php
@@ -3,28 +3,15 @@ declare(strict_types=1);
 
 namespace OrderComponent\Service\Outbox;
 
-use Doctrine\ORM\EntityManagerInterface;
-use OrderComponent\Repository\Outbox\OutboxMessageRepository;
 use OrderComponent\Messenger\Message\OutboxDispatchedMessage;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final class OutboxPublisher
 {
-    public function __construct(
-        private OutboxMessageRepository $repo,
-        private EntityManagerInterface $em,
-        private MessageBusInterface $bus
-    ) {}
+    public function __construct(private MessageBusInterface $bus) {}
 
-    public function replay(int $limit = 100): int
+    public function publish(string $topic, array $payload): void
     {
-        $count = 0;
-        foreach ($this->repo->findUnpublishedBatch($limit) as $msg) {
-            $this->bus->dispatch(new OutboxDispatchedMessage($msg->topic(), $msg->payload()));
-            $msg->markPublished();
-            $count++;
-        }
-        $this->em->flush();
-        return $count;
+        $this->bus->dispatch(new OutboxDispatchedMessage($topic, $payload));
     }
 }
