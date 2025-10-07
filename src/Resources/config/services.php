170eb397c1b1ea00dc7b8a2010597fb42cfb9166
Oleksandr Tishchenko <taa0662621456@gmail.com>
2025-10-07 00:52:12 -0500

    alpha iteration-Stabilization

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -2,13 +2,19 @@
 declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Reference;
-use OrderComponent\Controller\{OrderMetricsController, HealthCheckController};
+use OrderComponent\Controller\{ReadinessController};
+use OrderComponent\Command\{DlqRequeueCommand, OutboxReplayCommand};
 
 return static function (ContainerConfigurator $c): void {
     $s = $c->services()->defaults()->autowire()->autoconfigure();
-    $s->set(OrderMetricsController::class)->public();
-    $s->set(HealthCheckController::class)
-        ->public()
-        ->arg('$em', new Reference('doctrine.orm.entity_manager'))
-        ->arg('$asyncTransport', new Reference('messenger.transport.async'));
+    $s->set(ReadinessController::class)
+      ->public()
+      ->arg('$asyncTransport', new Reference('messenger.transport.async'));
+
+    $s->set(DlqRequeueCommand::class)
+      ->tag('console.command')
+      ->arg('$failed', new Reference('messenger.transport.failed'))
+      ->arg('$async', new Reference('messenger.transport.async'));
+
+    $s->set(OutboxReplayCommand::class)->tag('console.command');
 };
